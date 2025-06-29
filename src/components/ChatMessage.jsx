95f2945db10e301ba4e1e35a9a407573620be8be
Abduraxmon <2442abd72@gmail.com>
2025-06-29 15:11:45 +0500

    times style fixed

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -7,17 +7,21 @@ function ChatMessage(props) {
   // Xabarni yuborgan odam o'zimiz bo'lsak, "sent" klassini, bo'lmasa "received" klassini beramiz
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
+    // Xabarni yuborgan odam o'zimiz bo'lsak, "sentTime" klassini, bo'lmasa "receivedTime" klassini beramiz (Vaqtni togri korsatish uchun)
+  const messageTimeClass = uid === auth.currentUser.uid ? 'sentTime' : 'receivedTime';
+
 const timeString = createdAt
     ? new Date(createdAt.seconds * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
     : '';
+
   return (
     <div className={`message ${messageClass}`}>
       <div className="message-content">
         {/* Agar xabar bizniki bo'lmasa, yuboruvchi ismini ko'rsatamiz */}
         {messageClass === 'received' && <p className="sender-name">{displayName}</p>}
         <p className="text">{text}</p>
-        {createdAt && <span className="message-time">{timeString}</span>}
-      </div>
+      </div> 
+      {createdAt && <span className={`message-time ${messageTimeClass}`}>{timeString}</span>}
     </div>
   );
 }
